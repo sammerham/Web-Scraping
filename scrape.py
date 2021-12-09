1f039bd5d63adc80c1e2273b4bdc42af5cb16b78
Sam Merham <72219099+sammerham@users.noreply.github.com>
2021-12-08 23:50:44 -0800

    first commit

---
+++ scrape.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+URL = 'https://www.lendingtree.com/reviews/personal/first-midwest-bank/52903183'
+page = requests.get(URL)
+soup = BeautifulSoup(page.content, "html.parser")
+results = soup.find("section", {"class": "lenderReviews"})
+reviews_elements = results.find_all("div", class_="mainReviews")
+
+reviews = []
+for review_element in reviews_elements:
+    review = {}
+    review['review_title']= review_element.find("p", class_="reviewTitle").text .strip()
+    review['review_text'] = review_element.find("p", class_="reviewText").text .strip()
+    review['customer_name'] = review_element.find("p", class_="consumerName").text .strip()
+    review['review_date']= review_element.find("p", class_="consumerReviewDate").text .strip()
+    review['review_rating'] = review_element.find("div", class_="numRec").text .strip()
+    reviews.append(review)
+    print('hellooooooooo---->>>>>>>>>>>>', review)
+  
+
+# print('reviews------>>>>', reviews)
+reviews_json = json.dumps(reviews)
+print('reviews------>>>>', reviews_json)
+
+
+
+
+# query parametter for url to change lender
+# get request 
