c27cf4c41f30e20cdc3f8099b77d8ecc77a49def
Sam Merham <sammerham@icloud.com>
2021-12-14 16:42:49 -0800

    added more comments

--- app.py
+++ app.py
@@ -12,11 +12,14 @@ def show_reviews(lender_name, lender_id):
     Handles GET requests like /reviews/lender_name/lender_id
     Returns JSON format of Reviews for lender provided in url path
     """
-
+    # building url from url params and base url
     lender=f"{lender_name}/{lender_id}"
     url = f"{BASE_URL}{lender}"
     print(url)
-    reviews = get_reviews(url)  
-
-    return jsonify({'Reviews':reviews})
+    # get reviews from provided from url
+    reviews = get_reviews(url)
+    if('err' in reviews):
+        return jsonify({'Errors': reviews})
+    else:
+        return jsonify({'Reviews':reviews})
 
